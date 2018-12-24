14e8fbda149858f717e2227fd55802dc4d043890
Niraj Pandkar <niraj.pandkar@gmail.com>
2018-12-24 08:40:20 +0530

    Initial commit

---
+++ scrape_races.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import time
+import re
+# r = requests.get("http://www.typeracerdata.com/texts?texts=full&sort=id")
+# data = r.text
+
+start = time.time()
+with open("races.html" ,"r") as infile:
+    data = infile.read()
+soup = BeautifulSoup(data, 'html.parser')
+
+tables = soup.findAll('table')
+
+# There are 3 tables on the page. We want the third one only.
+table = tables[2]
+headers = [header.text for header in table.find_all('th')]
+print(headers)
+
+rows = list()
+# for row in table.find_all('tr'):
+#     rows.append([val.text for val in row.find_all('td')])
+
+for row in table.find_all('tr'):
+    content = list()
+    for i, col in enumerate(row.find_all('td')):
+        if col.a and i==4:
+            content.append(re.findall(r'id=([0-9]+)\&', col.a.get('href'))[0])
+        else:
+            content.append(col.text)
+    rows.append(content)
+
+with open("races.csv", "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(headers)
+    writer.writerows(row for row in rows if row)
+
+print("Time taken: {}".format(time.time()-start))
---
+++ scrape_texts.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import time
+
+# r = requests.get("http://www.typeracerdata.com/texts?texts=full&sort=id")
+# data = r.text
+
+start = time.time()
+with open("texts.html" ,"r") as infile:
+    data = infile.read()
+soup = BeautifulSoup(data, 'html.parser')
+
+table = soup.find('table')
+
+headers = [header.text for header in table.find_all('th')]
+print(headers)
+
+rows = list()
+for row in table.find_all('tr'):
+    rows.append([val.text for val in row.find_all('td')])
+
+with open("texts.csv", "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(headers)
+    writer.writerows(row for row in rows if row)
+
+print("Time taken: {}".format(time.time()-start))
